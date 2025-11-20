d57254f516c3cc1b5aabacd197ee33a434d56e74
naveenmv <naveenmvoff@gmail.com>
2025-11-20 21:15:00 +0530

    Error in merge - Need to check

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,9 +4,10 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: ["/","/packers-and-movers/contact", "/packers-and-movers/bangalore", "/packers-and-movers/chennai", "/packers-and-movers/coimbatore", "/packers-and-movers/kochi", "/packers-and-movers/thiruvananthapuram"],
+      allow: ["/", "/packers-and-movers/bangalore", "/packers-and-movers/chennai", "/packers-and-movers/coimbatore", "/packers-and-movers/kochi", "/packers-and-movers/thiruvananthapuram"],
       disallow: ["/admin", "/private", "/privacyPolicy", "/thank-you/order-placed"],
     },
     sitemap: "https://eintransport.in/sitemap.xml",
   };
 }
+
